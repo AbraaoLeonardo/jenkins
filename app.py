3852ef8010125669396cfcc25493708ed7d2ae39
abraao284 <abraao-284@outlook.com>
2024-02-13 08:53:09 -0300

    Adding the product route

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
+@app.route('/produtos', methods="['GET']")
+def products():
+    return "Carros"
 
 app.run(host='127.0.0.1', port=3000)
